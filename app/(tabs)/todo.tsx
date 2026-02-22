d0349b79c5e4fb1fe4e0bea19bacb30261d1c6b0
Ankush Mittal <ankush.mittal@au.mcd.com>
2026-02-22 14:24:39 +1100

    new tab

---
+++ app/(tabs)/todo.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AnimationScreen() {
+
+const Circle = ({ size = 100, color = 'black' }) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor: color,
+      }}
+    />
+  );
+};
+  return <SafeAreaView style={styles.Container}>
+    <Text>To DO App</Text>
+     <Circle />
+    </SafeAreaView>;
+// return <Text>Animation Screen</Text>;
+}
+
+
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  Container: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
